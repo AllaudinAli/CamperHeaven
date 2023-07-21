2b650d76cc2aa0ebd69fd1c942270a658293a5a8
AllaudinAli <ahmadcrisp@gmail.com>
2023-07-21 22:48:14 +0500

    transferred

---
+++ app.js
@@ -0,0 +1,102 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash')
+const mongoose = require('mongoose');
+const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const localStrategy = require('passport-local')
+const User = require('./models/user');
+
+//Routes
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRouters = require('./routes/users');
+
+//Mongoose Connection
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://0.0.0.0:27017/the-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+//Ejs Setup
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+
+
+
+const sessionConfig = {
+    secret: 'UnusualSecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session(sessionConfig))
+app.use(flash());
+
+//Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+//----------------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------------
+//Web Page
+
+app.use('/', userRouters)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+//Errors
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong'
+    res.status(statusCode).render('error', { err });
+})
+
+
+//----------------------------------------------------------------------------------
+
+app.listen(3000, () => {
+    console.log('Serving Port 3000')
+})
