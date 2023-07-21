b6552a253fbee58967e387d79c6e800ab75ea562
AllaudinAli <ahmadcrisp@gmail.com>
2023-07-22 01:11:27 +0500

    port

--- app.js
+++ app.js
@@ -169,5 +169,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Serving Port 3000')
+    console.log(`Serving Port ${port}`)
 })
